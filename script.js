56e728333f7082ce3a3c1cfaab937e3fd5bdf77a
coops-web <angadbhatia122@gmail.com>
2025-05-25 15:06:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 
 let alreadyExists = false;
 let alreadyExistsApp1 = false;
+let welcomeAppExists = false;
+let menuCreated = false;
 let count = 0;
+const isMobile = window.matchMedia("(max-width: 768px)").matches;
 const myList = [];
 
 class App {
@@ -33,13 +36,15 @@ class App {
         closeButton.addEventListener('mousedown', (e) => {
             e.stopPropagation();
             closeButton.style.backgroundColor = 'gray';
-            closeButton.addEventListener('mouseup', () => {
-                this.app.classList.remove('show');
-            })
-
-            document.addEventListener('mouseup', () => {
+            const outsideofclosebutton = () => {
                 closeButton.style.backgroundColor = 'rgb(220, 220, 220)';
-            })
+                document.removeEventListener('mouseup', outsideofclosebutton);
+            }
+            document.addEventListener('mouseup', outsideofclosebutton);
+        });
+
+        closeButton.addEventListener('click', () => {
+            this.app.classList.remove('show');
 
             this.app.addEventListener('transitionend', () => {
                 this.app.remove();
@@ -49,16 +54,20 @@ class App {
                     myList.splice(index, 1);
                 }
 
+                this.app.remove(); 
+
                 console.log(this.app.exists);
                 if(name == 'Wagabagabooboo') {
                     alreadyExists = false;
                 }
                 if(name == 'no') {
                     alreadyExistsApp1 = false;
-                }               
+                }
+                if(name == 'Welcome') {
+                    welcomeAppExists = false;
+                }
             });
-        });
-        
+        }, {once:true});
 
         this.header.appendChild(closeButton);
         this.app.appendChild(this.header);
@@ -75,12 +84,18 @@ class App {
     getExists() {
         return this.exists;
     }
+
+    getWidth() {
+        return width;
+    }
 }
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
     time();
+    welcomeApp();
+    menuCreation();
 })
 
 //  app creation and other functions below
@@ -96,21 +111,147 @@ function time() {
         if(hours-12 > 0) {
             timefornow = hours-12+":0"+minutes+" PM";
         }
-        if(hours-12 <= 0) {
+        else if(hours-12 < 0) {
             timefornow = hours+":0"+minutes+" AM";
         }
+        else {
+            timefornow = hours+":0"+minutes+" PM";
+        }
+
     }
     else {
         if(hours-12 > 0) {
-            timefornow = hours-12+":"+minutes+" PM";
+            timefornow = (hours-12) +":"+minutes+" PM";
         }
-        if(hours-12 <= 0) {
+        else if(hours-12 < 0) {
             timefornow = hours+":"+minutes+" AM";
+        }
+        else {
+            timefornow = hours+":"+minutes+" PM";
         }
     }
 
     document.querySelectorAll("#time")[0].innerHTML = timefornow;
     setTimeout(time, 1000);
+}
+
+
+function menuCreation() {
+
+    console.log("MENU CREATION CALLED");
+
+    let divcontainer = document.getElementById('menubar');
+    let startbutton = document.getElementById('startButton');
+
+    if(menuCreated) {
+        startbutton.addEventListener('mousedown', () => {
+            startbutton.style.backgroundColor = 'gray';
+
+            startbutton.addEventListener('mouseup', () => {
+                divcontainer.classList.remove('show');
+                menuCreated = false;
+            })
+            document.addEventListener('mouseup', () => {
+                startbutton.style.backgroundColor = 'rgb(220, 220, 220)';
+            })
+        })
+        return;
+    }
+
+    startbutton.addEventListener('mousedown', () => {
+        startbutton.style.backgroundColor = 'gray';
+
+        startbutton.addEventListener('mouseup', () => {
+            divcontainer.classList.add('show');
+            menuCreated = true;
+        })
+        document.addEventListener('mouseup', () => {
+            startbutton.style.backgroundColor = 'rgb(220, 220, 220)';
+        })
+    })  
+}
+
+function welcomeApp() {
+    if(welcomeAppExists) {
+        return;
+    }
+    let welcome = new App(450, 750, "Welcome", 100, 50);
+    if(isMobile) {
+        welcome = new App(200, 200, "Welcome", 100, 50); 
+    }
+
+    let viewondesktop = document.createElement('p');
+    viewondesktop.innerHTML = "For the best experience of this portfolio, please switch to the closest desktop near you. Some of the functionality is lost on a mobile device, though you can still see everything I've made.";
+    viewondesktop.style.fontSize = 10 + 'px';
+    viewondesktop.style.position = 'relative';
+    viewondesktop.style.top = 10 + 'px';
+    document.getElementById('test').appendChild(welcome.getApp());
+    let header1 = document.createElement('p');
+    header1.innerHTML = "Welcome to <b>Portfolio<span style='color:#fff'>06</span></b>";
+    header1.style.position = 'absolute';
+    header1.style.fontSize = 40 + 'px';
+    header1.style.left = 30 + 'px';
+
+    let video = document.createElement('div');
+    video.style.display = 'flex';
+    video.style.justifyContent = 'flex-start';
+    video.style.width =  '75%';
+    video.style.height = '75%';
+    video.style.borderTop =  '2px solid rgb(154, 154, 154)';
+    video.style.borderLeft =  '2px solid rgb(154, 154, 154)';
+    video.style.borderBottom =  '2px solid white';
+    video.style.borderRight = '2px solid white';
+    video.style.left = 30 + 'px';
+    if(isMobile) {
+        video.style.width = 50 + 'px';
+        video.style.height = 50 + 'px';
+    }
+    video.style.position = 'absolute';
+    
+    video.style.backgroundColor = 'rgb(250, 250, 209)';
+    video.style.top = 90 + 'px';
+
+    let didyouknow = document.createElement('p');
+    didyouknow.style.fontWeight = 'bold';
+    didyouknow.innerHTML = 'Did you know...';
+    didyouknow.style.fontSize = 25 + 'px';
+    didyouknow.style.position = 'absolute';
+    didyouknow.style.left = '10%';
+
+    let pressStart = document.createElement('p');
+    pressStart.style.fontSize = 20 + 'px';
+    pressStart.style.position = 'absolute';
+    pressStart.style.left = '10%';
+    pressStart.style.top = '15%';
+    pressStart.innerHTML = "You can press the start button on the bottom left to view the projects I've made, along with their Github repositories, if such repositories exist. IMAGE GOES BELOW.";
+    
+    video.appendChild(pressStart);
+    video.appendChild(didyouknow);
+    welcome.getApp().appendChild(video);
+
+    header1.style.fontSize = 25 + 'px';
+    if(isMobile) {
+        header1.style.fontSize = 12 + 'px';
+    }
+    let header06 = document.createElement('p');
+    header06.innerHTML = "06";
+    header06.style.fontFamily = "Sans-Serif";
+    header06.style.color = "white";
+
+    welcome.getApp().appendChild(header1);
+
+    if(isMobile) {
+        welcome.getApp().appendChild(viewondesktop);
+    }
+
+    myList.push(welcome.getApp());
+
+    requestAnimationFrame( () => {
+        welcome.getApp().classList.add('show');
+    });
+    welcomeAppExists = true;
+
+    doitall(welcome.getApp(), welcome.getHeader());
 }
 
 function app1() {
@@ -120,7 +261,7 @@ function app1() {
     const tryApp = new App(300,  500, "no", 230, 80);
 
     let tester23 = document.createElement('p');
-    tester23.innerHTML = "agjeawngiawengiawjegwiauegauwiegbnwajgjeawifigewajgeiowajgejiwaiojgeiowjifjsiojefoijseofjsoidjfisejfosjdiofjoeijfisoegjhoihogiehroijweoifjoijdfoisejfoisjehfiohsgioesojfiewjeoiwhgiowehgiosnefosneoifnhi";
+    tester23.innerHTML = "tester";
     tryApp.getApp().appendChild(tester23);
 
     let waves1 = document.createElement('div');
@@ -173,15 +314,69 @@ function createApp() {
 
 
 
-
-// main function to control movement
+// main function to control movement, resizing, etc.
 
 function doitall(app, header) {
+    let appRight = 0;
+    /*
+    * Had to consult AI and other resources for this issue. Turns out that my transition animation, where the
+    * windows were popping into existence was creating some issues. doitall was being immediately called 
+    * after the transition occured, so doitall would store the position of the right edge mid-way through
+    * its transition. To fix this, we just wait until the transition ends, then save the position of the
+    * right edge.
+    */
+    app.addEventListener('transitionend', () => {
+        appRight = app.getBoundingClientRect().right;
+        console.log("RIGHT: "+ appRight);
+
+        appBottom = app.getBoundingClientRect().bottom;
+    })
+
     console.log("doitall called");
     let offsetX = 0;
     let offsetY = 0;
-    
+
+
+    app.addEventListener('mousedown', (e) => {
+        offsetX = e.clientX - app.offsetLeft;
+        offsetY = e.clientY - app.offsetTop;
+        console.log(offsetX);
+        if(offsetX >= app.getBoundingClientRect().width-10) {
+            document.addEventListener('mousemove', resizeRight);
+            document.addEventListener('mouseup', stop);
+        }
+
+        if(offsetY >= app.getBoundingClientRect().height-10) {
+            document.addEventListener('mousemove', resizeBottom);
+            document.addEventListener('mouseup', stop);
+        }
+
+        if(offsetX <= 10) {
+            document.addEventListener('mousemove', resizeLeft);
+            document.addEventListener('mouseup', stop);
+        }
+    })
+
     header.addEventListener('mousedown', (e) => {
+
+        
+        offsetX = e.clientX - app.offsetLeft;
+        offsetY = e.clientY - app.offsetTop;
+        if(offsetX >= app.getBoundingClientRect().width-5) {
+            document.addEventListener('mousemove', resizeRight);
+            document.addEventListener('mouseup', stop);
+        }
+
+        if(offsetX <= 5) {
+            document.addEventListener('mousemove', resizeLeft);
+            document.addEventListener('mouseup', stop);
+        }
+
+        if(offsetY <= 5) {
+            document.addEventListener('mousemove', resizeTop);
+            document.addEventListener('mouseup', stop);
+        }
+
         for(let i = 0; i < myList.length; i++) {
             if(myList[i] == app) {
                 app.style.zIndex = 10;
@@ -190,15 +385,27 @@ function doitall(app, header) {
                 myList[i].style.zIndex = 5;
             }
         }
-        offsetX = e.clientX - app.offsetLeft;
-        offsetY = e.clientY - app.offsetTop;
-        document.addEventListener('mousemove', move);
-        document.addEventListener('mouseup', stop);
+
+        if(!(offsetX >= app.getBoundingClientRect().width-5) && !(offsetX < 5) && !(offsetY < 5)) {
+            document.addEventListener('mousemove', move);
+            document.addEventListener('mouseup', stop);
+        }
         
     });
     
     function move(e) 
     {
+        if(app.getBoundingClientRect().width >= window.innerWidth && app.getBoundingClientRect().height >= window.innerHeight-40) {
+            console.log("true");
+            app.style.width = 400 + 'px';
+            app.style.height = 400 + 'px';
+            offsetX = app.offsetLeft;
+            offsetY = app.offsetTop;
+            const headerChild = app.children[0];
+            const closeButtonChild = headerChild.children[0];
+    
+            closeButtonChild.style.left = parseInt(app.style.width) - 25 + 'px';
+        }
         let maxHorizontal = window.innerWidth - app.getBoundingClientRect().width;
         let maxVertical = window.innerHeight - app.getBoundingClientRect().height;
         
@@ -206,12 +413,67 @@ function doitall(app, header) {
         app.style.top = Math.max(0, Math.min(e.clientY - offsetY, maxVertical-40)) + 'px';
         
         console.log(app.style.left);
+
+        appRight = app.getBoundingClientRect().right;
+        appBottom = app.getBoundingClientRect().bottom;
     }
     function stop() 
     {
         document.removeEventListener('mousemove', move);
         document.removeEventListener('mouseup', stop);
+        document.removeEventListener('mousemove', resizeRight);
+        document.removeEventListener('mousemove', resizeBottom);
+        document.removeEventListener('mousemove', resizeLeft);
+        document.removeEventListener('mousemove', resizeTop);   
     }
+
+    function resizeRight(e) {
+        let appLeft = app.getBoundingClientRect().left;
+        let maxWidth = window.innerWidth - appLeft;
+
+        app.style.width = Math.max(300, Math.min(e.clientX - appLeft, maxWidth)) + 'px';
+
+        const headerChild = app.children[0];
+        const closeButtonChild = headerChild.children[0];
+    
+        closeButtonChild.style.left = parseInt(app.style.width) - 25 + 'px';
+        appRight = app.getBoundingClientRect().right;
+    }
+    
+    function resizeBottom(e) {
+        let appTop = app.getBoundingClientRect().top;
+        let maxHeight = window.innerHeight - appTop - 40;
+
+        app.style.height = Math.max(300, Math.min(e.clientY - appTop, maxHeight)) + 'px';
+
+        appBottom = app.getBoundingClientRect().bottom;
+    }
+
+
+    function resizeLeft(e) {
+        if(appRight - (e.clientX - offsetX) > 300) {
+            app.style.left = Math.max(0, e.clientX - offsetX) + 'px'; 
+        }
+        app.style.right = appRight;
+
+        app.style.width = Math.max(300, appRight - parseInt(app.style.left)) + 'px';
+
+        const headerChild = app.children[0];
+        const closeButtonChild = headerChild.children[0];
+    
+        closeButtonChild.style.left = parseInt(app.style.width) - 25 + 'px';
+    }
+
+    function resizeTop(e) {
+        if(appBottom - (e.clientY - offsetY) > 300) {
+            app.style.top = Math.max(0, e.clientY - offsetY) + 'px';
+        }
+        app.style.bottom = appBottom;
+
+        app.style.height = Math.max(300, appBottom - parseInt(app.style.top)) + 'px';
+
+    }
+
 }
 
 /*
